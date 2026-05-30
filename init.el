6491ae7fa5cbff7e060bfe1b4516b9d73e02813e
zuoxucheng <zuoxucheng@gmail.com>
2026-05-31 08:35:22 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,26 @@
+;; -*- lexical-binding: t; -*-
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(require 'use-package)
+
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((gptel-quick :url "https://github.com/karthink/gptel-quick")
+     (xdg-launcher :url "https://github.com/emacs-exwm/xdg-launcher"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
