3aa074d1bbfe4ed98dd78eebddf2a69c3e38775a
zuoxucheng <zuoxucheng@gmail.com>
2026-06-15 15:23:51 +0900

    [python]initial python workflow setup

--- init.el
+++ init.el
@@ -15,11 +15,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values '((eval when (fboundp 'rainbow-mode) (rainbow-mode 1))))
- '(package-selected-packages
-   '(avy denote gptel-quick gruvbox-theme jinx kdl-mode magit marginalia
-	 multi-vterm multiple-cursors org-journal org-modern
-	 org-modern-indent org-roam popper ruff-format toc-org tramp
-	 uniline uv-mode vertico visual-fill-column xdg-launcher))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
    '((gptel-quick :url "https://github.com/karthink/gptel-quick")
      (xdg-launcher :url "https://github.com/emacs-exwm/xdg-launcher"))))
