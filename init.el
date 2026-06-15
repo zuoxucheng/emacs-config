80ebd4ff33aab343358a156dbe286d03e91f91f5
zuoxucheng <zuoxucheng@gmail.com>
2026-06-15 19:39:23 +0900

    [python]adjustments for better workflow

--- init.el
+++ init.el
@@ -15,7 +15,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values '((eval when (fboundp 'rainbow-mode) (rainbow-mode 1))))
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(apheleia avy code-cells comint-mime company corfu denote gptel-quick
+	      gruvbox-theme jinx jupyter kdl-mode lsp-bridge lsp-mode
+	      magit marginalia multi-vterm multiple-cursors
+	      org-journal org-modern org-modern-indent org-roam popper
+	      ruff-format toc-org uniline uv-mode vertico
+	      visual-fill-column xdg-launcher yasnippet
+	      yasnippet-snippets))
  '(package-vc-selected-packages
    '((gptel-quick :url "https://github.com/karthink/gptel-quick")
      (xdg-launcher :url "https://github.com/emacs-exwm/xdg-launcher"))))
